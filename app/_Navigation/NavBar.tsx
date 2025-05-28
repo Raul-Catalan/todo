2f06c0e42beb0e8a388affa7fcb61ee926195f64
Raul-Catalan <raulrcatalan@gmail.com>
2025-05-27 22:16:35 -0700

    Add GitHub Provider, Add Custom SignIn Page Components

--- app/_Navigation/NavBar.tsx
+++ app/_Navigation/NavBar.tsx
@@ -1,30 +1,25 @@
+import SignIn from "@/components/auth/sign-in";
 import { Button } from "@/components/ui/button";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "@/components/ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 
 export default function NavBar() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          Link
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          Link
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          Link
-        </NavigationMenuItem>
+        <NavigationMenuItem>Link</NavigationMenuItem>
+        <NavigationMenuItem>Link</NavigationMenuItem>
+        <NavigationMenuItem>Link</NavigationMenuItem>
       </NavigationMenuList>
       <NavigationMenuList>
         <NavigationMenuItem>
-          <Button>
-            Login
-          </Button>
+          <Button>Login</Button>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <Button>
-            Sign Up
-          </Button>
+          <SignIn />
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
---
+++ app/signin/page.tsx
@@ -0,0 +1,2 @@
+import { redirect } from "next/navigation";
+import { signIn, auth, providerMap } from "@/auth";
